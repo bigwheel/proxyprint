157f2c0bc6555c5a57e8cc05e57caa3efab01ea1
bigwheel <k.bigwheel+eng@gmail.com>
2012-10-28 20:36:24 +0900

    とりあえず最初の一つ目のカードを取得できるようにした
     * 次にするべきは、すべてのカード情報を取得するところ

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
@@ -9,7 +11,10 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def card_images = Action {
-    Ok("baby")
+  def card_images = Action { implicit request =>
+    val cardlistForm = Form(tuple("multiverseid[0]" -> number, "number_of_card[0]" -> number))
+    val (mid, noc) = cardlistForm.bindFromRequest.get
+
+    Ok(mid.toString + " " + noc.toString)
   }
 }
