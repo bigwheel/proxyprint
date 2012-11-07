6f888188f11ab0594a4bb53af86bc7c9ed59ef3a
bigwheel <k.bigwheel+eng@gmail.com>
2012-11-07 19:40:18 +0900

    onClickのイベントをやめてjQueryによるイベントに変更した

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,27 +1,25 @@
-function onButtonClick() {
-    $.ajax({
-        url: 'http://mtgbase.herokuapp.com/search',
-        data: { card_name: $("#card_name").val() },
-        dataType: 'json'
-    }).success(function(data) {
-        var select_dialog = $("#select_dialog");
-
-        _.each(data, function(card_property) {
-            var image_tag = "<img class='card_image' src='<%= src %>' multiverseid='<%= multiverseid %>'>";
-            select_dialog.append(_.template(image_tag, {src: card_property.card_image_url, multiverseid: card_property.multiverseid}));
-            $("#select_dialog").dialog({width: 800, height: 550});
-        });
-    });
-    return false;
-}
-
 $(document).ready(function(){
     var search_dialog = $("#search_dialog");
     var select_dialog = $("#select_dialog");
+
+    $("#card_search_button").click(function() {
+        $.getJSON(
+            'http://mtgbase.herokuapp.com/search',
+            { card_name: $("#card_name").val() }
+        ).done(function(data) {
+            _.each(data, function(card_property) {
+                var image_tag = "<img class='card_image' src='<%= src %>' multiverseid='<%= multiverseid %>'>";
+                select_dialog.append(_.template(image_tag, {src: card_property.card_image_url, multiverseid: card_property.multiverseid}));
+                select_dialog.dialog({width: 800, height: 550});
+            });
+        });
+    });
+
     $(".card_search_button").live("click", function(event) {
         search_dialog.dialog({width: 800, height: 550});
         search_dialog.data('index', $(this).attr('index'));
     });
+
     $(".card_image").live("click", function(event) {
         select_dialog.dialog("close");
         search_dialog.dialog("close");
